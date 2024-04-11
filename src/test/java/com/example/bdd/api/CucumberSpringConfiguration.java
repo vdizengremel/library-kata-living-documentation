1418f5a559bb5e4bc87447c972edb3f358d95e86
vdizengremel <vdizengremel@octo.com>
2024-04-11 11:34:54 +0200

    remove unused file

--- src/test/java/com/example/bdd/api/CucumberSpringConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.example.bdd.api;
-
-//@ContextConfiguration(classes = DemoApplication.class)
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties =
-//        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration,org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration")
-//@ActiveProfiles({"inMemoryRepository"})
-//@AutoConfigureMockMvc
-public class CucumberSpringConfiguration {
-}
