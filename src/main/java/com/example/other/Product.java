7aa15fe63d0269adb8c290c08ad2afb1b8af3f14
vdizengremel <vdizengremel@octo.com>
2024-09-11 21:27:56 +0200

    remove unused code

--- src/main/java/com/example/other/Product.java
+++
@@ -1,9 +0,0 @@
-package com.example.other;
-
-public class Product {
-    private Price price;
-
-    void setPrice(Price price) {
-        this.price = price;
-    }
-}
--- src/main/java/com/example/other/ProductRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.other;
-
-public interface ProductRepository {
-    Product findProductById(int id);
-    void save(Product product);
-}
